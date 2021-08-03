171d8b49f3b0dff0ba01f92a0f0a1180cf18c5d3
IrfanShah <71603574+DeveloperIrkhan@users.noreply.github.com>
2021-08-03 15:29:01 +0500

    Search by Fname and Lname coomplete

--- my-ts-app/src/App.tsx
+++ my-ts-app/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { EmployeeRecord } from './Pages/EmployeeRecord';
+import { SearchUser } from './Pages/SearchUser';
 
 function App() {
   return (
     <div className="App">
-     <EmployeeRecord/>
+     <div className="container my-5"><EmployeeRecord/></div>
+     <div className="container my-5"><SearchUser/> </div>
     </div>
   );
 }
--- my-ts-app/src/Pages/SearchUser.tsx
+++ my-ts-app/src/Pages/SearchUser.tsx
@@ -2,53 +2,77 @@ import React from 'react'
 import Moke_Data from './MOCK_DATA.json'
 interface Props { }
 
-interface State { }
+interface State {
+    Name: string;
+}
 
 export class SearchUser extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
-        this.state = {};
+        this.state = {
+            Name: "",
+        };
     }
     render() {
-        var studentFullName="irfan shah Aakhunzada";
-        var details=[]
-        details=studentFullName.split(' ');
+        var studentFullName = "irfan shah Aakhunzada";
+        var details = []
+        details = studentFullName.split(' ');
+        const { Name } = this.state;
         return (
-        <div className="">
-            <h2>Your Full name is <span className="text-danger">{studentFullName}</span></h2>
-            <h4>your First Name is <span className="text-success"> {details[0]}</span></h4>
-            <h4>your Middle Name is <span className="text-success"> {details[1]}</span></h4>
-            <h4>your Last Name is <span className="text-success">{details[2]}</span></h4>
+            <div className="">
+                <div className="col-4 m-auto">
+                    <input
+                        type="text"
+                        onChange={(e) => {
+                            this.setState({ Name: e.target.value })
+                        }}
+                        className="form-control"
+                        placeholder="Search....."
+                    /></div>
+                <div className="container p-5">
+                    <table className="table table-bordered">
+                        <thead>
+                            <tr><th>Id</th>
+                                <th>First Name</th>
+                                <th>Last Name</th>
+                                <th>Email </th>
+                                <th>Gender</th>
+                                <th>Phone No</th></tr>
+                        </thead>
+                        <tbody>
+                            {
+                                Moke_Data.filter((value) => {
+                                    if (Name === "") {
+                                        return value;
+                                    }
+                                    else if (value.first_name.toLocaleLowerCase().includes(Name.toLocaleLowerCase()) ||
+                                        value.last_name.toLocaleLowerCase().includes(Name.toLocaleLowerCase())) {
+                                        return value;
+                                    }
 
-                <div className="col-4 m-auto">  <input type="text" className="form-control" placeholder="Enter for Search!" /></div>
-            <div className="container p-5">
-            <table className="table table-bordered">
-                    <thead>
-                        <tr><th>Id</th>
-                            <th>First Name</th>
-                            <th>Last Name</th>
-                            <th>Email </th>
-                            <th>Gender</th>
-                            <th>IP Address</th></tr>
-                    </thead>
-                    <tbody>
-                        {
-                            Moke_Data.map((data, key) => {
-                                return (
-                                <tr key={data.id}>
-                                    <td>{data.id}</td>
-                                    <td>{data.first_name}</td>
-                                    <td>{data.last_name}</td>
-                                    <td>{data.email}</td>
-                                    <td>{data.gender}</td>
-                                    <td>{data.ip_address}</td>
-                                </tr>)
-                                console.log(data)
-                            })
-                        }
-                    </tbody>
-                </table>
-            </div>
+                                }).map((data, key) => {
+                                        return (
+                                            <tr key={data.id}>
+                                                <td>{data.id}</td>
+                                                <td>{data.first_name}</td>
+                                                <td>{data.last_name}</td>
+                                                <td>{data.email}</td>
+                                                <td>{data.gender}</td>
+                                                <td>{data.PhoneNo}</td>
+                                            </tr>)
+                                        console.log(data)
+                                    })
+                            }
+                        </tbody>
+                    </table>
+                </div>
+                <div className="py-5">
+                <h2>Your Full name is <span className="text-danger">{studentFullName}</span></h2>
+                <h4>your First Name is <span className="text-success"> {details[0]}</span></h4>
+                <h4>your Middle Name is <span className="text-success"> {details[1]}</span></h4>
+                <h4>your Last Name is <span className="text-success">{details[2]}</span></h4>
+
+                </div>
             </div>
         )
     }
